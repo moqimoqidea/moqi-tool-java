4892f2e1cb4ccac65981c87126526c88c71a6d03
moqi <moqimoqidea@gmail.com>
2020-03-04 15:30:24 +0800

    测试 1.2.66 版本的 fastjson 解析异常

---
+++ src/test/java/com/moqi/bean/PersonTest.java
@@ -0,0 +1,24 @@
+package com.moqi.bean;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.parser.Feature;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author moqi
+ * On 3/4/20 10:49
+ */
+class PersonTest {
+
+    /**
+     * 1.2.66 版本的 fastjson 解析异常
+     */
+    @Test
+    public void parse() {
+        String line = "{\"name\":5, \"address\":\"beijing\", \"age\": 10}";
+
+        Person bean = JSON.parseObject(line, Person.class, Feature.InitStringFieldAsEmpty);
+        System.out.println("bean = " + bean);
+    }
+
+}
