1df75109edac5df9372f200aa699b0b7dea98c63
moqi <moqimoqidea@gmail.com>
2021-02-20 10:42:27 +0800

    练习 Java 8 java.util.function.Function Tutorial with Examples

---
+++ src/main/java/com/moqi/bean/Employee.java
@@ -0,0 +1,18 @@
+package com.moqi.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * Employee Bean
+ *
+ * @author moqi On 2/19/21 14:22
+ */
+@Data
+@AllArgsConstructor
+public class Employee {
+
+    private String name;
+    private int age;
+
+}
