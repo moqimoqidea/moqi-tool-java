6e722ecc10fdfeb686e885c3ce185294da9b900e
moqi <moqimoqidea@gmail.com>
2021-04-28 14:58:54 +0800

    Add JSONCheck

---
+++ src/main/java/com/moqi/json/JSONCheck.java
@@ -0,0 +1,60 @@
+package com.moqi.json;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONException;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * JSON 工具类
+ *
+ * Created by moqi
+ * On 4/21/21 14:18
+ */
+@Slf4j
+public class JSONCheck {
+
+    public static void main(String[] args) {
+
+        boolean validJSON = isValidJSON("123");
+        System.out.println("validJSON = " + validJSON);
+
+        boolean validJSON1 = isValidJSON("");
+        System.out.println("validJSON1 = " + validJSON1);
+
+        boolean validJSONArray = isValidJSONArray("");
+        System.out.println("validJSONArray = " + validJSONArray);
+
+        boolean validJSONArray1 = isValidJSONArray("[]");
+        System.out.println("validJSONArray1 = " + validJSONArray1);
+
+    }
+
+    public static boolean isValidJSON(String string) {
+        try {
+            JSON.parseObject(string);
+        } catch (JSONException exception) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static boolean isNotJSON(String string) {
+        return !isValidJSON(string);
+    }
+
+    public static boolean isValidJSONArray(String string) {
+        try {
+            JSON.parseArray(string);
+        } catch (JSONException exception) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static boolean isNotJSONArray(String string) {
+        return !isValidJSONArray(string);
+    }
+
+}
