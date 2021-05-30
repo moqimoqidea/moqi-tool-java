2144ac5aa5d82002c45b5bfb3631af26c4883681
moqi <moqimoqidea@gmail.com>
2021-05-30 19:33:08 +0800

    学习 BeanUtils 功能

---
+++ src/main/java/com/moqi/in20200530/beanutil/Employee.java
@@ -0,0 +1,48 @@
+package com.moqi.in20200530.beanutil;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 学习 BeanUtils 功能
+ *
+ * @author moqi
+ * On 2021/5/30 17:48
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String firstName;
+
+    private String lastName;
+
+    private List<Employee> subordinate;
+
+    private Map<String, Address> address;
+
+    @Data
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Address implements Serializable {
+
+        private static final long serialVersionUID = 1L;
+
+        private String name;
+
+    }
+
+}
+
+
---
+++ src/test/java/com/moqi/in20200530/beanutil/EmployeeTest.groovy
@@ -0,0 +1,126 @@
+package com.moqi.in20200530.beanutil
+
+
+import com.google.common.collect.Lists
+import org.apache.commons.beanutils.PropertyUtils
+import spock.lang.Specification
+
+/**
+ * 测试 BeanUtils 功能
+ *
+ * @author moqi* On 2021/5/30 18:23
+ */
+class EmployeeTest extends Specification {
+
+    def "property utils simple should work"() {
+        given:
+        def employee = Employee.builder().firstName("first").build()
+
+        when:
+        def firstName = PropertyUtils.getSimpleProperty(employee, "firstName")
+        def lastName = PropertyUtils.getSimpleProperty(employee, "lastName")
+        PropertyUtils.setSimpleProperty(employee, "firstName", "someNewFirstName")
+        PropertyUtils.setSimpleProperty(employee, "lastName", "lastName")
+
+        then:
+        firstName == "first"
+        lastName == null
+        employee.firstName == "someNewFirstName"
+        employee.lastName == "lastName"
+    }
+
+    def "property utils indexed should work"() {
+        given:
+        def employee = Employee.builder()
+                .subordinate(Lists.newArrayList(
+                        Employee.builder().lastName("sub1LastName").build(),
+                        Employee.builder().firstName("sub2FirstName").build()
+                ))
+                .build()
+
+        when:
+        def index = 0
+        def name = "subordinate[" + index + "]"
+        def employeeS1 = (Employee) PropertyUtils.getIndexedProperty(employee, name)
+        def employeeS2 = (Employee) PropertyUtils.getIndexedProperty(employee, "subordinate", 1)
+        PropertyUtils.setSimpleProperty(employee, "subordinate", Lists.newArrayList(
+                Employee.builder().build(),
+                Employee.builder().build(),
+                Employee.builder().build(),
+                Employee.builder().build()
+        ))
+        PropertyUtils.setIndexedProperty(employee, "subordinate[2]",
+                Employee.builder().firstName("sub3FirstName").build()
+        )
+        PropertyUtils.setIndexedProperty(employee, "subordinate", 3,
+                Employee.builder().firstName("sub4FirstName").build()
+        )
+
+        then:
+        employeeS1.lastName == "sub1LastName"
+        employeeS2.firstName == "sub2FirstName"
+        employee.subordinate.size() == 4
+        employee.subordinate[2].getFirstName() == "sub3FirstName"
+        employee.subordinate[3].getFirstName() == "sub4FirstName"
+    }
+
+    def "property utils mapped should work"() {
+        given:
+        def employee = Employee.builder()
+                .address(new HashMap<String, Employee.Address>() {
+                    {
+                        put("home", Employee.Address.builder().name("home").build())
+                    }
+                })
+                .build()
+
+        when:
+        def homeAddress = PropertyUtils.getMappedProperty(employee, "address(home)")
+        def homeAddress2 = PropertyUtils.getMappedProperty(employee, "address", "home")
+        PropertyUtils.setMappedProperty(employee, "address", "company",
+                Employee.Address.builder().name("company").build()
+        )
+        PropertyUtils.setMappedProperty(employee, "address(company123)",
+                Employee.Address.builder().name("company123").build()
+        )
+
+        then:
+        homeAddress == Employee.Address.builder().name("home").build()
+        homeAddress2 == homeAddress
+        employee.address.size() == 3
+        employee.address["company"] == Employee.Address.builder().name("company").build()
+        employee.address["company123"] == Employee.Address.builder().name("company123").build()
+    }
+
+    def "property utils mapped replace map field should work"() {
+        given:
+        def employee = Employee.builder()
+                .address(Collections.singletonMap("home", Employee.Address.builder().name("home").build()))
+                .build()
+
+        when:
+        PropertyUtils.setSimpleProperty(employee, "address",
+                Collections.singletonMap("company", Employee.Address.builder().name("company").build())
+        )
+
+        then:
+        employee.address.size() == 1
+        employee.address["company"] == Employee.Address.builder().name("company").build()
+    }
+
+    def "property utils nested should work"() {
+        given:
+        def employee = Employee.builder()
+                .address(Collections.singletonMap("home", Employee.Address.builder().name("New York").build()))
+                .build()
+
+        when:
+        def homeName = PropertyUtils.getNestedProperty(employee, "address(home).name",)
+        PropertyUtils.setNestedProperty(employee, "address(home).name", "Los Angeles")
+
+        then:
+        homeName == "New York"
+        employee.getAddress().get("home").name == "Los Angeles"
+    }
+
+}
