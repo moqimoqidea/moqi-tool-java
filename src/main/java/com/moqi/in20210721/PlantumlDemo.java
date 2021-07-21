f04185409beb4ebd4b0ddf0969ec3a254deec6a6
moqi <moqimoqidea@gmail.com>
2021-07-21 12:42:59 +0800

    Add PlantumlDemo.java

---
+++ src/main/java/com/moqi/in20210721/PlantumlDemo.java
@@ -0,0 +1,19 @@
+package com.moqi.in20210721;
+
+import net.sourceforge.plantuml.Run;
+
+import java.io.IOException;
+
+/**
+ * 练习 Plantuml
+ *
+ * @author moqi
+ * @date 7/21/21 11:28
+ */
+public class PlantumlDemo {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Run.main(new String[]{"-tsvg", "src/main/resources/flowYaml.yaml"});
+    }
+
+}
